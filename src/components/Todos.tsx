53907a6f81d4973d464c029f9caae9545b034ee2
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2023-05-02 22:39:42 +0330

    14-working with props and typescript

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-
-const Todos = () => {
-    return (
-        <ul>
-            <li>Learn React</li>
-            <li>Learn Typescript</li>
-        </ul>
-    );
+const Todos: React.FC<{ items: string[]; children?: React.ReactNode }> = (
+  props
+) => {
+  return (
+    <ul>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default Todos;
